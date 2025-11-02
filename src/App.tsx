00d0baef6f0496883e9fb7c32a710a6d9f5bb937
lruizrodr <lruiz0002@gmail.com>
2025-11-02 12:09:02 -0600

    hash router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import TodoPage from "./components/pages/TodoPage";
 import ContactPage from "./components/pages/ContactPage";
@@ -8,17 +8,16 @@ import "./App.css";
 function App() {
   return (
     <div className="app-container">
-      <BrowserRouter>
-        <NavBar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Navigate to="/todos" replace />} />
-            <Route path="/todos" element={<TodoPage />} />
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/project" element={<ProjectPage />} />
-          </Routes>
-        </main>
-      </BrowserRouter>
+      <HashRouter>
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path="/" element={<TodoPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/project" element={<ProjectPage />} />
+        </Routes>
+      </main>
+    </HashRouter>
     </div>
   );
 } 
